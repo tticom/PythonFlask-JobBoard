668658af33b2a192b636204c3ea5abbbed570d1f
Niall Finlay <niall.finlay@gmail.com>
2022-05-18 15:04:23 +0100

    module 7

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,8 @@
 from email.policy import default
 from re import T
-from flask import Flask, render_template, g
+from flask import Flask, redirect, render_template, g, request
 import sqlite3
+import datetime
 
 PATH = "db/jobs.sqlite"
 
@@ -48,3 +49,14 @@ def employer(employer_id):
   reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
   return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
   
+@app.route('/review/<employer_id>/review', methods=('GET', 'POST'))
+def review(employer_id):
+  if request.mthod == 'POST':
+    review  = request.form['review']
+    rating = request.form['rating']
+    title = request.form['title']
+    status = request.form['status']
+    date = datetime.datetime.now().strftime("%m/%d/%Y")
+    return redirect(url_for('employer', employer_id=employer_id))
+  execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+  return render_template('review.html', employer_id=employer_id)
